e262a0d10497e1f6efbe7cb09725cb66ce896399
JollyGrin <lol@dean.lol>
2025-05-10 18:38:42 +0200

    more

--- src/routes/card/+page.svelte
+++ src/routes/card/+page.svelte
@@ -2,13 +2,18 @@
   import Card from "$lib/Card/Card.svelte";
   import { DATA_CARDS } from "./data-cards";
 
-  const cards = DATA_CARDS.slice(0, 5);
+  let place = $state(0);
+  const cards = $derived(DATA_CARDS.slice(0 + place, 6 + place));
 </script>
 
+<button
+  class="bg-blue-300 p-2 ml-5 z-50 absolute"
+  onclick={() => (place = place + 6)}
+>
+  more {place}
+</button>
 <div class="w-screen h-screen grid grid-cols-3 p-2">
   {#each cards as card (card.imageName)}
-    {#key card.name}
-      <Card {card} />
-    {/key}
+    <Card {card} />
   {/each}
 </div>
